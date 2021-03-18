a3ee53ba6de184abdd0e919e78f61effbe77e972
oluwadareseyi <oluwadareseyii@gmail.com>
2021-03-18 09:09:11 +0100

    add event and listeners in figma and component

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>
-  <h1>Plugin</h1>
-  </div>
+  <main class="c-main">
+    <h1>Plugin</h1>
+    <button class="button c-button" @click="buttonClick">Click me</button>
+  </main>
 </template>
 
 <script>
-import "../figma/figma-ds/js/selectMenu";
-import "../figma/figma-ds/js/iconInput";
-import "../figma/figma-ds/js/disclosure";
+import '../figma/figma-ds/js/selectMenu';
+import '../figma/figma-ds/js/iconInput';
+import '../figma/figma-ds/js/disclosure';
 
-import Welcome from "./components/Welcome";
+import Welcome from './components/Welcome';
 
 export default {
   data() {
@@ -25,10 +26,31 @@ export default {
     window.selectMenu.init();
     window.iconInput.init();
     window.disclosure.init();
+
+    window.onmessage = event => {
+      const { type, name, data } = event.data.pluginMessage;
+
+      //  recieve data
+      if (type === 'event-name') {
+        console.log('event called');
+      }
+    };
+  },
+  methods: {
+    buttonClick() {
+      parent.postMessage(
+        {
+          pluginMessage: {
+            type: 'event-click'
+          }
+        },
+        '*'
+      );
+    }
   }
 };
 </script>
 
 <style lang="scss">
-@import "../figma/figma-ds/figma-plugin-ds";
+@import '../figma/figma-ds/figma-plugin-ds';
 </style>
